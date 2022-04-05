dc3c39e3ede11534fd5d7b60cbc149727fd6b4c2
seoh02h <seok02h@khu.ac.kr>
2022-04-05 13:26:45 +0900

    order-service 추가

---
+++ e-commerce/order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
@@ -0,0 +1,46 @@
+package com.example.orderservice.jpa;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name="orders")
+public class OrderEntity implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 120, unique = true)
+  private String productId;
+
+  /**
+   * 수량
+   */
+  @Column(nullable = false)
+  private Integer qty;
+
+  /**
+   * 단가
+   */
+  @Column(nullable = false)
+  private Integer unitPrice;
+
+  @Column(nullable = false)
+  private Integer totalPrice;
+
+  @Column(nullable = false)
+  private String userId;
+
+  @Column(nullable = false, unique = true)
+  private String orderId;
+
+  @Column(nullable = false, updatable = false, insertable = false)
+  @ColumnDefault(value = "CURRENT_TIMESTAMP")
+  private Date createdAt;
+}
